b92907c8b595f874ec1bd171d5bdf022c3d672bb
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2023-10-04 13:57:59 +0500

    Yangi IWork interfaceni qo'shdim ,IUserInterfacega ham ayrim qo'shimchalar qo'shdim.

--- src/ExFourProject/Interface/IUserInterface.cs
+++ src/ExFourProject/Interface/IUserInterface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExFourProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace ExFourProject.Interface
 {
-    internal interface IUserInterface
+    public interface IUserInterface
     {
+        public Task<bool> CreateAsync(Users user);
+        public Task<string> GetNameByIDAsync(int id);
+
     }
 }
